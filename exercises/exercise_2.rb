1854dc7016d49eb9d2bd4f0111ac378fd6cf84a3
samjanderson <sam.andersonnn@live.ca>
2021-03-01 20:39:40 +0000

    complete find, update and destroy store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,16 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# @store1 = Store.find_by(id: 1)
+# @store2 = Store.find_by(id: 2)
+
+# @store1.update(name: "Yaletown")
+
+#ALTERNATE SYNTAX
+#simpler because you can find based on primary key
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+#alternate way to update
+@store1.name = "Yaletown"
+@store1.save
